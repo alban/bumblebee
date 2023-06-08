d37fd74f4ceeb892ea58055141620f80646d7f94
Alban Crequy <albancrequy@linux.microsoft.com>
2023-06-08 17:36:59 +0200

    Add examples based on bpf iterators
    Compiled with:
    
    make install-cli
    rm -f examples/snapshot-file/snapshot-file.o
    bee build examples/snapshot-file/snapshot-file.c albantest.azurecr.io/snapshot-file:dev -i ghcr.io/solo-io/bumblebee/builder:0.0.9
    bee push albantest.azurecr.io/snapshot-file:dev
    
    Signed-off-by: Alban Crequy <albancrequy@linux.microsoft.com>

---
+++ examples/snapshot-file/snapshot-file.c
@@ -0,0 +1,48 @@
+#include "vmlinux.h"
+#include "solo_types.h"
+#include "bpf/bpf_helpers.h"
+#include "bpf/bpf_core_read.h"
+#include "bpf/bpf_tracing.h"
+
+char __license[] SEC("license") = "Dual MIT/GPL";
+
+const volatile bool show_threads = false;
+
+SEC("iter/task")
+int ig_snap_proc(struct bpf_iter__task *ctx)
+{
+	struct seq_file *seq = ctx->meta->seq;
+	__u32 seq_num = ctx->meta->seq_num;
+	__u64 session_id = ctx->meta->session_id;
+	struct task_struct *task = ctx->task;
+	struct task_struct *parent;
+	pid_t parent_pid;
+	static struct btf_ptr ptr = {};
+
+	if (!task)
+		return 0;
+
+	if (!show_threads && task->tgid != task->pid)
+		return 0;
+
+	__u64 mntns_id = task->nsproxy->mnt_ns->ns.inum;
+
+	parent = task->real_parent;
+	if (!parent)
+		parent_pid = -1;
+	else
+		parent_pid = parent->pid;
+
+	__u32 uid = task->cred->uid.val;
+	__u32 gid = task->cred->gid.val;
+
+	BPF_SEQ_PRINTF(seq, "%d %d %d %llu %d %d %s\n",
+		task->tgid, task->pid, parent_pid, mntns_id, uid, gid, task->comm);
+
+        ptr.type_id = bpf_core_type_id_kernel(struct user_namespace);
+        ptr.ptr = BPF_CORE_READ(task, cred, user_ns);
+
+	bpf_seq_printf_btf(seq, &ptr, sizeof(ptr), 0);
+
+	return 0;
+}
